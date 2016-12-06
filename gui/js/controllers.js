35657321878184320f563b8ec1821d2735868b2a
pham91 <ethanphamcs@gmail.com>
2016-12-06 13:30:10 -0500

    added login

--- gui/js/controllers.js
+++ gui/js/controllers.js
@@ -1,6 +1,26 @@
+
+//controller for login
+app.controller('login', function($scope, $http) {
+  $scope.submitLogin = function () {
+      console.log($scope.form);
+      $http.post('/login', $scope.form).
+      success(function(data) {
+        //go to homepage.html
+        console.log(data);
+      });
+      window.location.href="template/homepage.html";
+    };
+});
+//controller for peer review
+
+
+
+//-----------------------------------------------------------------------------
+// UPDATE controller
+//-----------------------------------------------------------------------------
 //controller for Update Schedule Start time
 app.controller('scheduleStartTime', function($scope, $http) {
-    $scope.submitValue = function () {
+    $scope.urgentExec = function () {
       var r = confirm("Are you sure want to update?");
       if (r == true) {
         $http.put('/update/scheduleStartTime', $scope.form).
@@ -15,13 +35,14 @@ app.controller('scheduleStartTime', function($scope, $http) {
 });
 // controller for Update Status Code
 app.controller('statusCode', function($scope, $http) {
-  $scope.submitValue = function () {
+  $scope.urgentExec = function () {
     var r = confirm("Are you sure want to update?");
     if (r == true) {
     $http.put('/update/statusCode', $scope.form).
       success(function(data) {
         $scope.banner = JSON.stringify(data, null, 2);;
       });
+
       return true;
     } else {
       return false;
@@ -30,7 +51,7 @@ app.controller('statusCode', function($scope, $http) {
 });
 //controller for Valuation End Date
 app.controller('valuationEnd', function($scope, $http) {
-  $scope.submitValue = function () {
+  $scope.urgentExec = function () {
     var r = confirm("Are you sure want to update?");
     if (r == true) {
     $http.put('/update/valuationEnd', $scope.form).
@@ -45,7 +66,7 @@ app.controller('valuationEnd', function($scope, $http) {
 });
 //controller for Valuation Start time
 app.controller('valuationStart', function($scope, $http) {
-  $scope.submitValue = function () {
+  $scope.urgentExec = function () {
     var r = confirm("Are you sure want to update?");
     if (r == true) {
     $http.put('/update/valuationStart', $scope.form).
@@ -58,24 +79,26 @@ app.controller('valuationStart', function($scope, $http) {
     }
   };
 });
-//controller for SLA Date and Time by Audit
+
+//DEMO controller for SLA Date and Time by Audit
 app.controller('sla_by_audit', function($scope, $http) {
-  $scope.submitValue = function () {
-    var r = confirm("Are you sure want to update?");
-    if (r == true) {
-    $http.put('/update/sla_by_audit', $scope.form).
-      success(function(data) {
-        $scope.banner = JSON.stringify(data, null, 2);
-      });
-      return true;
-    } else {
-      return false;
+  $scope.urgentExec = function () {
+      var r = confirm("Are you sure want to update?");
+      if (r == true) {
+      $http.put('/update/sla_by_audit', $scope.form).
+        success(function(data) {
+          $scope.banner = JSON.stringify(data, null, 2);
+        });
+        return true;
+      } else {
+        return false;
+      }
     }
-  };
 });
+
 //controller for SLA Date and Time by run name
 app.controller('sla_by_runname', function($scope, $http) {
-  $scope.submitValue = function () {
+  $scope.urgentExec = function () {
     var r = confirm("Are you sure want to update?");
     if (r == true) {
     $http.put('/update/sla_by_runname', $scope.form).
@@ -90,7 +113,7 @@ app.controller('sla_by_runname', function($scope, $http) {
 });
 //controller for  Historical SLA Date and Time
 app.controller('histoy_SLA', function($scope, $http) {
-  $scope.submitValue = function () {
+  $scope.urgentExec = function () {
     var r = confirm("Are you sure want to update?");
     if (r == true) {
     $http.put('/update/histoy_SLA', $scope.form).
@@ -105,7 +128,7 @@ app.controller('histoy_SLA', function($scope, $http) {
 });
 //controller for Run Status Code by Run Name and Group Number
 app.controller('status_name_grpNumder', function($scope, $http) {
-  $scope.submitValue = function () {
+  $scope.urgentExec = function () {
     var r = confirm("Are you sure want to update?");
     if (r == true) {
     $http.put('/update/status_name_grpNumder', $scope.form).
@@ -120,7 +143,7 @@ app.controller('status_name_grpNumder', function($scope, $http) {
 });
 //controller for Run Status Code by Run Name and Driver Step Detail ID
 app.controller('status_name_dtlID', function($scope, $http) {
-  $scope.submitValue = function () {
+  $scope.urgentExec = function () {
     var r = confirm("Are you sure want to update?");
     if (r == true) {
     $http.put('/update/status_name_dtlID', $scope.form).
@@ -135,7 +158,7 @@ app.controller('status_name_dtlID', function($scope, $http) {
 });
 //controller for Active Step Indicator by Driver Step ID
 app.controller('active_step_indicator_stepID', function($scope, $http) {
-  $scope.submitValue = function () {
+  $scope.urgentExec = function () {
     var r = confirm("Are you sure want to update?");
     if (r == true) {
     $http.put('/update/active_step_indicator_stepID', $scope.form).
@@ -150,7 +173,7 @@ app.controller('active_step_indicator_stepID', function($scope, $http) {
 });
 //controller for Update Active Step Indicator by Run Name and Driver Step ID
 app.controller('active_step_indicator_runName_stepID', function($scope, $http) {
-  $scope.submitValue = function () {
+  $scope.urgentExec = function () {
     var r = confirm("Are you sure want to update?");
     if (r == true) {
     $http.put('/update/active_step_indicator_runName_stepID', $scope.form).
@@ -165,7 +188,7 @@ app.controller('active_step_indicator_runName_stepID', function($scope, $http) {
 });
 //controller for Update Active Step Indicator by Run Name
 app.controller('active_step_indicator_runName', function($scope, $http) {
-  $scope.submitValue = function () {
+  $scope.urgentExec = function () {
     var r = confirm("Are you sure want to update?");
     if (r == true) {
     $http.put('/update/active_step_indicator_runName', $scope.form).
@@ -180,7 +203,7 @@ app.controller('active_step_indicator_runName', function($scope, $http) {
 });
 //controller for Update Active Step Indicator by Run Name and Group Number
 app.controller('active_step_indicator_runName_grpNumber', function($scope, $http) {
-  $scope.submitValue = function () {
+  $scope.urgentExec = function () {
     var r = confirm("Are you sure want to update?");
     if (r == true) {
     $http.put('/update/active_step_indicator_runName_grpNumber', $scope.form).
@@ -192,4 +215,76 @@ app.controller('active_step_indicator_runName_grpNumber', function($scope, $http
       return false;
     }
   };
+});
+
+//-----------------------------------------------------------------------------
+// DELETE controllers
+//-----------------------------------------------------------------------------
+//controller for Delete Driver Schedule
+app.controller('Dl_Driver_Schedule', function($scope, $http) {
+    $scope.urgentExec = function () {
+      $http.put('/delete/driverSchedule', $scope.form).
+        success(function(data) {
+          $scope.banner = data
+        });
+    };
+});
+//controller for Delete Driver Step
+app.controller('Dl_Driver_Step_RunName_GrpNbr', function($scope, $http) {
+    $scope.urgentExec = function () {
+      $http.put('/delete/Driver_Step_RunName_GrpNbr', $scope.form).
+        success(function(data) {
+          $scope.banner = data
+        });
+    };
+});
+//controller for Delete Driver Step
+app.controller('Dl_Driver_Step_RunName', function($scope, $http) {
+    $scope.urgentExec = function () {
+      $http.put('/delete/Driver_Step_RunName', $scope.form).
+        success(function(data) {
+          $scope.banner = data
+        });
+    };
+});
+//controller for Delete Driver Step
+app.controller('Dl_Driver_Step_RunName_Sid', function($scope, $http) {
+    $scope.urgentExec = function () {
+      $http.put('/delete/Driver_Step_RunName_Sid', $scope.form).
+        success(function(data) {
+          $scope.banner = data
+        });
+    };
+});
+//controller for Delete Driver Step Detail
+app.controller('Dl_Driver_Step_Detail_RunName', function($scope, $http) {
+    $scope.urgentExec = function () {
+      $http.put('/delete/Driver_Step_RunName_Sid', $scope.form).
+        success(function(data) {
+          $scope.banner = data
+        });
+    };
+});
+
+//-----------------------------------------------------------------------------
+// ADD controllers
+//-----------------------------------------------------------------------------
+//controller for Add Driver Scedule
+app.controller('DriverSchedule', function($scope, $http) {
+    $scope.urgentExec = function () {
+      $http.put('/add/DriverSchedule', $scope.form).
+        success(function(data) {
+          $scope.banner = data
+        });
+    };
+});
+
+//controller for Add Driver Step
+app.controller('DriverStep', function($scope, $http) {
+    $scope.urgentExec = function () {
+      $http.put('/add/DriverStep', $scope.form).
+        success(function(data) {
+          $scope.banner = data
+        });
+    };
 });
--- server/server.js
+++ server/server.js
@@ -2,13 +2,50 @@ var express  = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var sqlite3 = require('sqlite3').verbose();
-//your database location
+
+//database location
 var db = new sqlite3.Database("/Users/thanhpham/Downloads/LibertyMutual.db");
+//user table
+var users = new sqlite3.Database("/Users/thanhpham/Downloads/Users.db");
+users.serialize(function() {
+    users.run("CREATE TABLE IF NOT EXISTS user_info (id INT PRIMARY KEY, username TEXT, password TEXT, email TEXT, type TEXT)");
+    users.run("INSERT OR REPLACE INTO user_info VALUES (?,?,?,?,?)", [1, 'developer', 'abcd', '@gmail.com', 'developer']);
+    users.run("INSERT OR REPLACE INTO user_info VALUES (?,?,?,?,?)", [2, 'admin', 'abcd', '@gmail.com', 'administrator']);
+});
 
-app.use(bodyParser.urlencoded({'extended':'true'}));
+// //MAILGUN service
+// var api_key = 'key-a63b9592560ea2c4f5dabe2dc8c53682';
+// var domain = 'sandbox5535bd649fbc436193cba0471dc6d85d.mailgun.org';
+// var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+
+
+
+
+//Config middleware
+app.use(bodyParser.urlencoded({ extended : true }));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + "/../gui"));
+app.use(express.static(__dirname + '/../gui'));
 
+
+
+
+//-----------------------------------------------------------------------------
+// MACROS Routes
+//-----------------------------------------------------------------------------
+
+//Login Logout
+app.post('/login', function(req, res) {
+  users.serialize(function() {
+    users.all("SELECT * FROM user_info WHERE username = '" + req.body.username + "' AND password = '" + req.body.password + "' ", function(err, rows){
+        if(err){
+          console.log("Fail authenticate");
+        }
+        else {
+          console.log(rows);
+        }
+    });
+  });
+});
 
 
 app.put('/update/scheduleStartTime', function(req, res) {
@@ -91,24 +128,42 @@ app.put('/update/valuationStart', function(req, res) {
   });
 });
 
+//DEMO
 app.put('/update/sla_by_audit', function(req, res) {
-  db.serialize(function() {
-    db.all("UPDATE C_DRIVER_SCHEDULE SET sla_date = '" + req.body.sla_dt + "', sla_time = '" + req.body.sla_time + "' WHERE audit_id = " + req.body.auditId + " ", function(err){
-      if(err){
-        res.send("Error when querrying");
-      }
-      else {
-        db.all("SELECT * FROM C_DRIVER_SCHEDULE WHERE audit_id = " + req.body.auditId + " ", function(err, rows){
-          if(err){
-            res.send("Error response");
-          }
-          else{
-            res.send(rows);
-          }
-        });
-      }
+  // if (true) {
+  //   var data = {
+  //     from: 'Digital Dash <postmaster@sandbox5535bd649fbc436193cba0471dc6d85d.mailgun.org>',
+  //     to: 'serobnic@mail.ru',
+  //     subject: 'Hello',
+  //     text: req.body
+  //   };
+  //   mailgun.messages().send(data, function (error, body) {
+  //     if(error){
+  //       res.send("Mail not sent. Error occured");
+  //     }
+  //     else{
+  //       res.send("Your request for Update SLA Date and Time has been sent to administrator");
+  //     }
+  //   });
+  // }
+  // else {
+    db.serialize(function() {
+      db.all("UPDATE C_DRIVER_SCHEDULE SET sla_date = '" + req.body.sla_dt + "', sla_time = '" + req.body.sla_time + "' WHERE audit_id = " + req.body.auditId + " ", function(err){
+        if(err){
+          res.send("Error when querrying");
+        }
+        else {
+          db.all("SELECT * FROM C_DRIVER_SCHEDULE WHERE audit_id = " + req.body.auditId + " ", function(err, rows){
+            if(err){
+              res.send("Error response");
+            }
+            else{
+              res.send(rows);
+            }
+          });
+        }
+      });
     });
-  });
 });
 
 app.put('/update/sla_by_runname', function(req, res) {
@@ -156,6 +211,7 @@ app.put('/update/status_name_grpNumder', function(req, res) {
   });
 });
 
+//DEMO
 app.put('/update/status_name_dtlID', function(req, res) {
   db.serialize(function() {
     db.all("UPDATE C_DRIVER_STEP_DETAIL SET run_stts_cd = '" + req.body.Status + "' WHERE run_name = '" + req.body.runName + "' AND drvr_step_dtl_id = " + req.body.dtl_id + " ", function(err){
