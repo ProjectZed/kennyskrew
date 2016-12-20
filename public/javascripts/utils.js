a6215d9dbb1f21fc47afbbc302c27091eaeffa83
elvis4617 <chongzechen@umass.edu>
2016-12-20 03:58:03 -0500

    hide and show banner on update fixed styling difference between update pages

--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -1,1 +1,23 @@
+function redBorder(element){
+    document.getElementById(element).style.borderColor = "red";
+}
+function removeRedBorder(element){
+    document.getElementById(element).style.borderColor = "#EBE9ED";
+}
 
+function hideButton(permission){
+  var exec = document.getElementById('exec');
+  var urgentExec = document.getElementById('urgentExec');
+  if(permission != "administrator")
+    urgentExec.style.display = "none";
+  else
+    exec.style.display = "none";
+}
+
+function hideBanner(){
+  document.getElementById('error-banner').style.display = "none";
+}
+
+function showBanner(){
+  document.getElementById('error-banner').style.display = "block";
+}
